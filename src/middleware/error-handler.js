e2531f21241184c3f5b3cc9e860b079f797bf67f
Nixa Zaiter <linkclone30@gmail.com>
2026-07-30 20:11:27 -0500

    Testing

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,7 +1,16 @@
 module.exports = (err, req, res, next) => {
+  console.log({
+    name: err.name,
+    statusCode: err.statusCode,
+    message: err.message,
+  });
+
   console.error(err);
+
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
+    name: err.name,
+    statusCode,
     message: statusCode === 500 ? "An error occurred on the server" : message,
   });
 };
